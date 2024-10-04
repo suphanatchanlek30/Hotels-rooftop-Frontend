baeefede8607305ae2042f437b08fb14d5197cc6
suphanatchanlek <suphanatchanlek@gmail.com>
2024-10-04 16:28:51 +0700

    add Make Navbar Mobile Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NavLink} from "react-router-dom"
+import { IoMenuSharp } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const navLists = [
     {name: "Home", path: "/"},
@@ -9,7 +11,11 @@ const navLists = [
 ]
 
 const Navbar = () => {
-  return (
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+    return (
     <header className='bg-white py-6 border'>
         <nav className='container mx-auto flex justify-between px-5'>
             {/* Logo */}
@@ -33,7 +39,43 @@ const Navbar = () => {
                     <NavLink to={"/login"}>Login</NavLink>
                 </li>
             </ul>
+
+            {/* toggle menu */}
+            <div className='flex items-center sm:hidden'>
+                <button
+                onClick={toggleMenu}
+                className='flex items-center px-3 py-4 bg-[#fafafa] rounded text-sm text-gray-500 hover:text-gray-900'>
+                    {
+                        isMenuOpen ? <IoClose className='size-6' />: <IoMenuSharp className='size-6' />
+                    }
+                </button>
+            </div>
         </nav>
+
+        {/* mobile menu items */}
+        {
+            isMenuOpen && (
+                <ul className='fixed top-[108px] left-0 w-full h-auto pb-8 border-b bg-white shadow-sm z-50'>
+                {
+                    navLists.map((list, index) => (
+                        <li className='mt-5 px-4'>
+                            <NavLink
+                            // เมื่อกดเลือกแล้วจะปิด menu ให้เอง
+                            onClick={()=> setIsMenuOpen(false)}
+                            to={`${list.path}`}
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                              }
+                            >{list.name}</NavLink>
+                        </li>
+                    ))
+                }
+                    <li className='px-4 mt-5'>
+                        <NavLink to={"/login"}>Login</NavLink>
+                    </li>
+                </ul>
+            )
+        }
     </header>
   )
 }
